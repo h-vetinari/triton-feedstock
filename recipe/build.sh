b81978cc90a10367dda65ce28845f9439421654e
Michał Górny <mgorny@gentoo.org>
2025-02-18 07:09:12 +0100

    Switch to TRITON_OFFLINE_BUILD

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,18 +10,9 @@ export LLVM_SYSPATH=$PREFIX
 export PYBIND11_SYSPATH=$SP_DIR/pybind11
 
 # these don't seem to be actually used, but they prevent downloads
-export TRITON_PTXAS_PATH=$PREFIX/bin/ptxas
-export TRITON_CUOBJDUMP_PATH=$PREFIX/bin/cuobjdump
-export TRITON_NVDISASM_PATH=$PREFIX/bin/nvdisasm
-export TRITON_CUDACRT_PATH=$PREFIX
-export TRITON_CUDART_PATH=$PREFIX
-export TRITON_CUPTI_PATH=$PREFIX
+export TRITON_OFFLINE_BUILD=1
 
 export MAX_JOBS=$CPU_COUNT
-
-# the build does not run C++ unittests, and they implicitly fetch gtest
-# no easy way of passing this, not really worth a whole patch
-sed -i -e '/TRITON_BUILD_UT/s:\bON:OFF:' CMakeLists.txt
 
 # LLVM currently does not provide a way to override the tablegen executables,
 # effectively forcing the value of MLIR_TABLEGEN_EXE obtained
